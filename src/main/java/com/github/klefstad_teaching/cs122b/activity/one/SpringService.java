efd230f05c102a4f7c4e4952afbd6cc8e081b6e0
AlexanderIskander <Alex.Iskander@Gmail.com>
2022-03-25 11:36:58 -0700

    Added postman

--- src/main/java/com/github/klefstad_teaching/cs122b/activity/one/SpringService.java
+++ src/main/java/com/github/klefstad_teaching/cs122b/activity/one/SpringService.java
@@ -1,9 +1,14 @@
 package com.github.klefstad_teaching.cs122b.activity.one;
 
+import com.github.klefstad_teaching.cs122b.activity.one.config.SpringServiceConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({
+    SpringServiceConfig.class
+})
 public class SpringService
 {
     public static void main(String[] args)
--- src/main/java/com/github/klefstad_teaching/cs122b/activity/one/rest/HelloController.java
+++ src/main/java/com/github/klefstad_teaching/cs122b/activity/one/rest/HelloController.java
@@ -1,8 +1,12 @@
 package com.github.klefstad_teaching.cs122b.activity.one.rest;
 
+import com.github.klefstad_teaching.cs122b.activity.one.config.SpringServiceConfig;
 import com.github.klefstad_teaching.cs122b.activity.one.model.request.HelloFavoriteRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,6 +17,29 @@ import java.util.Optional;
 @RestController
 public class HelloController
 {
+    private final SpringServiceConfig config;
+
+    /**
+     * One of the main advantages of spring is its dependency injections. We will not need to go
+     * into much of this in this course but one thing we will request from this is the config file
+     * we created. Our {@link SpringServiceConfig} has an annotation on it {@link
+     * ConfigurationProperties} which means Spring will automatically create a single instance of
+     * this class and will map all of our custom config properties into it. When we want to use it
+     * all we have to do is have a constructor with the {@link Autowired} annotation. In a class
+     * marked as either {@link RestController} or {@link Component} with our {@link
+     * SpringServiceConfig} as one of the arguments.
+     * <p>
+     * <b>Note:</b> Using {@link Autowired} only works when you let spring create the instance for
+     * you. It will not work if you create the instance for your.
+     *
+     * @param config an instance of SpringServiceConfig
+     */
+    @Autowired
+    public HelloController(SpringServiceConfig config)
+    {
+        this.config = config;
+    }
+
     /**
      * Standard Endpoint that takes no inputs and returns a simple {@link ResponseEntity} with a
      * body of type {@link String}
@@ -93,5 +120,18 @@ public class HelloController
     {
         return ResponseEntity.status(HttpStatus.OK)
                              .body(request.toMessage());
+    }
+
+    /**
+     * Standard Endpoint that takes no inputs and returns a simple {@link ResponseEntity} with a
+     * body of type {@link String} with the default message found in config.
+     *
+     * @return ResponseEntity
+     */
+    @GetMapping("/hello/default")
+    public ResponseEntity<String> helloDefault()
+    {
+        return ResponseEntity.status(HttpStatus.OK)
+                             .body(config.getDefaultHello());
     }
 }
